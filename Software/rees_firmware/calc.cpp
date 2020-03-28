e4f1e7fa58771bb616173c5baf133c3105b1c7b0
Juan Piñera <info@pimakerovd.com>
2020-03-28 20:09:52 +0100

    Remove outdated and unused functions from calc

--- Software/rees_firmware/calc.cpp
+++ Software/rees_firmware/calc.cpp
@@ -19,44 +19,6 @@ int calcularVolumenTidal(int estatura, int sexo) {
   return ((int)(round(pesoIdeal * DEFAULT_ML_POR_KG_DE_PESO_IDEAL)));
 }
 
-
-
-/**
- * @brief Constrains the value within the limits
- */
-float constrainFloat(float value, float lowLimit, float highLimit) {
-  if (value < lowLimit) {
-    value = lowLimit;
-  } else if (value > highLimit) {
-    value = highLimit;
-  }
-  return value;
-}
-
-/**
- * PID step calculation
- */
-float proportional = 0;
-static float integral = 0;
-float derivative = 0;
-float previous_feedbackInput = 0;
-float computePID(float setpoint, float feedbackInput) {
-  //dt is set to TIME_BASE msec by timer interrupt
-
-  float error = setpoint - feedbackInput;
-
-  proportional = PID_KP * error;
-  integral += PID_KI * error;
-  integral = constrainFloat(integral, PID_MIN, PID_MAX);
-  derivative = -PID_KD * (feedbackInput - previous_feedbackInput);
-
-  float output = constrainFloat((proportional + integral + derivative), PID_MIN, PID_MAX);
-
-  // Guardar ultimo tiempo y error
-  previous_feedbackInput = feedbackInput;
-  return output;
-}
-
 /**
  * @brief Refresca el WDT (Watch Dog Timer)
  */
@@ -68,14 +30,6 @@ void refreshWatchDogTimer() {
 //   //TODO implementar
 //   return flow;
 // }
-
-
-float pos2vol(float position) { //converts position [steps] to volume [ml]
-  float volume = position * K_POS2VOL;
-
-  return volume;
-}
-
 
 void integratorFlowToVolume(float* currentVolume, float currentFlow) {
   //We add to currentVolume the ml that flowed in TIME_BASE msec;
@@ -108,31 +62,3 @@ float computeLPF(int parameter, int lpfArray[])
   return result;
 }
 #endif
-
-//float curveInterpolator(int[] curve, float min, float max, float currentProgressFactor);
-//float curveInterpolator(float maxValue, float currentProgressFactor);
-float curveInterpolator(float inValue, float currentProgressFactor) {
-	float outValue = 0;
-
-	if (currentProgressFactor < 0.03) {
-		outValue = 0.7 * inValue;
-	} else if ((currentProgressFactor >= 0.03) && (currentProgressFactor < 0.97))  {
-		outValue = 1.2 * inValue;
-	} else if (currentProgressFactor >= 0.97)  {
-		outValue = 0.7 * inValue;
-	}
-		
-	return outValue;
-}
-
-float flow2speed(float flow) { //converts flow [LPM] to speed [steps/sec]
-  //float speed  = flow *  (1/60) * (1000/1) * (1/800)                 * (100/1);
-  //     [steps/s]  [l/m]  [min/sec] [ml/l]    [fully_pressed_ambu/ml]   [steps/fully_pressed_ambu]
-  //float position  = volume * (25/12);
-  //  #define K_VOL2POS (STEPS_FOR_TOTALLY_PRESSED_AMBU / VOLUME_FOR_TOTALLY_PRESSED_AMBU)
-  //  K_FLOW2SPEED = (25/12);
-  
-  float speed = flow * K_FLOW2SPEED;
-
-  return speed;
-}
--- Software/rees_firmware/calc.h
+++ Software/rees_firmware/calc.h
@@ -21,20 +21,10 @@
 int calcularVolumenTidal(int estatura, int sexo);
 
 /**
- * @brief Constrains the value within the limits
- */
-float constrainFloat(float value, float lowLimit, float highLimit);
-
-float computePID(float setpoint, float feedbackInput);
-/**
  * @brief Refresca el WDT (Watch Dog Timer)
  */
 void refreshWatchDogTimer();
 
-float pos2vol(float position);
-float flow2speed(float flow);
-int integratorFlowToVolume(float currentFlow);
 void integratorFlowToVolume(float* currentVolume, float currentFlow);
-float curveInterpolator(float inValue, float currentProgressFactor);
 
 #endif // CALC_H
